750291d6c952841c92bdcff14db109709f33968b
alicbm <abarandica1234@gmail.com>
2023-03-23 11:48:52 -0500

    welcomeView ready for smarthphone screen

--- src/InputSearch/index.tsx
+++ src/InputSearch/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './InputSearch.css'
 
 export const InputSearch = () => {
   return (
--- src/WelcomeView/index.tsx
+++ src/WelcomeView/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { InputSearch } from '../InputSearch';
+import './WelcomeView.css'
 
 export const WelcomeView = () => {
   return (
