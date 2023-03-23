723915951484a3420c77fbbd8903e828a2a214cc
alicbm <abarandica1234@gmail.com>
2023-03-23 11:23:49 -0500

    header ready for smarthphone screen

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import logo from '../images/pelisflix_logo.png'
-import {TfiMenu} from 'react-icons/tfi'
+import { TfiMenu } from 'react-icons/tfi'
+import './Header.css'
 
 export const Header = () => {
+  const [modal, setModal] = React.useState<boolean>(false)
+
   return (
     <div className='Header'>
       <div className='Header-logo'>
         <img src={logo} alt="Pelisflix logo" />
       </div>
-      <ul className='Header-categories'>
+      <ul className={modal ? 'Header-categories --modal-active' : 'Header-categories'}>
         <li>Popular</li>
         <li>Now Playing</li>
         <li>Top Rated</li>
       </ul>
-      <div className='Header-menu'>
+      <div 
+        onClick={() => setModal(!modal)}
+        className='Header-menu'>
         <TfiMenu />
       </div>
     </div>
