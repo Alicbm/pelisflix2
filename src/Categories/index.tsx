94acd0ea24febb3474afb3efd4a52c933fbe6551
alicbm <abarandica1234@gmail.com>
2023-03-24 06:26:26 -0500

    categories ready for smarthphone screen

--- src/Categories/index.tsx
+++ src/Categories/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Categories.css'
 
 export const Categories = () => {
   return (
     <div className='Categories'>
       <h2 className='Categories-title'>Categories</h2>
-      <ul className='Categories-lits'>
+      <ul className='Categories-list'>
         <li>Action</li>
         <li>Action</li>
         <li>Action</li>
