6c960bdb2420198a5a7c5cc8ac0ce20a8f9f4a6d
alicbm <abarandica1234@gmail.com>
2023-03-26 17:44:58 -0500

    structure of Review component ready

---
+++ src/Reviews/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Reviews = () => {
+  return (
+    <div className='Reviews'>
+      <div className='Reviews-user'>
+        <div className='Reviews-user__image'>
+          <img src="" alt="" />
+        </div>
+        <div className='Reviews-user__user'>
+          <h2>A review by alicmejia008</h2>
+          <p>Created by alicmejia008, on Dec 08</p>
+        </div>
+      </div>
+      <div className='Reviews-comment'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quidem expedita vero repellendus sapiente vel adipisci! Dolore culpa recusandae minima molestiae ut, rerum deleniti delectus unde velit magni quae expedita.</p>
+      </div>
+    </div>
+  )
+}
