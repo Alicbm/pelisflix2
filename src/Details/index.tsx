7c283dc7cf3d7c47c2090ddd8172ab2f16be7bd1
alicbm <abarandica1234@gmail.com>
2023-03-27 13:07:38 -0500

    details page: design modified

--- src/Details/index.tsx
+++ src/Details/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { BsFillCalendarCheckFill } from 'react-icons/bs'
-import { BiCameraMovie } from 'react-icons/bi'
 import { useAppSelector } from '../store/hooks'
 import { TypeCategories } from '../types'
 import './Details.css'
@@ -9,6 +7,15 @@ export const Details = () => {
   const { movieSelected, categories } = useAppSelector(state => state.mainReducer)
   const url_image = process.env.REACT_APP_URL_IMAGE;
   const categoriesReleted: string[] = []
+
+  const formatDate = (date: string) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString("en-Us", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  };
 
   const styleBack = {
     background: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.8)), url(https://image.tmdb.org/t/p/original${movieSelected.backdrop_path})`,
@@ -24,23 +31,23 @@ export const Details = () => {
         }
       })
     })
-  }  
+  }    
 
   return (
     <div className='Details' key={movieSelected.id}>
-      <div className='Details-image'>
-        <img src={`${url_image}${movieSelected.poster_path}`} alt="" />
-      </div>
       <div className='Details-back' style={styleBack}>
         <h1 className='Details-back__title'>{movieSelected.title}</h1>
         <div className='Details-back__items'>
+          <img src={`${url_image}${movieSelected.poster_path}`} alt="" />
           <div className='Details-back__items-item'>
-            <span><BsFillCalendarCheckFill /></span>
-            <p>{movieSelected.release_date}</p>
+            <p>{formatDate(movieSelected.release_date)}</p>
           </div>
           <div className='Details-back__items-item'>
-            <span><BiCameraMovie /></span>
-            <p>{movieSelected.adult ? 'Suitable for children' : 'Not suitable for children'}</p>
+            <p>{
+              movieSelected.adult === false ? 
+              <span>Suitable for children</span> 
+              : <span>Not suitable for children</span>
+              }</p>
           </div>
           <div className='Details-back__items-popularity'>
             <p>Popularity</p>
